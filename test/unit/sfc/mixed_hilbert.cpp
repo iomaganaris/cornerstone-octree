5e2a4691ea04c8ddd5edc52b5edff41f040cc1e2
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-09-14 08:30:05 +0200

    Added x10y10z9 MixedHilbertBox test

--- test/unit/sfc/mixed_hilbert.cpp
+++ test/unit/sfc/mixed_hilbert.cpp
@@ -79,11 +79,70 @@ TEST(MixedHilbertBox, x10y9z9)
 TEST(MixedHilbertBox, x10y10z9)
 {
     unsigned bx = 10, by = 10, bz = 9;
+    int numKeys{10};
+    std::mt19937 gen;
+
+    std::uniform_int_distribution<unsigned> distribution_x_le_511(0, (1 << (bx - 1)) - 1); // 0 to 511
+    std::uniform_int_distribution<unsigned> distribution_x_ge_512(512, (1 << bx) - 1);     // 512 to 1023
+    std::uniform_int_distribution<unsigned> distribution_y_le_511(0, (1 << (by - 1)) - 1); // 0 to 511
+    std::uniform_int_distribution<unsigned> distribution_y_ge_512(512, (1 << by) - 1);     // 512 to 1023
+    std::uniform_int_distribution<unsigned> distribution_z(0, (1 << bz) - 1);
+
+    auto getRandXle511 = [&distribution_x_le_511, &gen]() { return distribution_x_le_511(gen); };
+    auto getRandXge512 = [&distribution_x_ge_512, &gen]() { return distribution_x_ge_512(gen); };
+    auto getRandYle511 = [&distribution_y_le_511, &gen]() { return distribution_y_le_511(gen); };
+    auto getRandYge512 = [&distribution_y_ge_512, &gen]() { return distribution_y_ge_512(gen); };
+    auto getRandZ      = [&distribution_z, &gen]() { return distribution_z(gen); };
+
+    std::vector<unsigned> x_le_511(numKeys);
+    std::vector<unsigned> x_ge_512(numKeys);
+    std::vector<unsigned> y_le_511(numKeys);
+    std::vector<unsigned> y_ge_512(numKeys);
+    std::vector<unsigned> z(numKeys);
+
+    std::generate(begin(x_le_511), end(x_le_511), getRandXle511);
+    std::generate(begin(x_ge_512), end(x_ge_512), getRandXge512);
+    std::generate(begin(y_le_511), end(y_le_511), getRandYle511);
+    std::generate(begin(y_ge_512), end(y_ge_512), getRandYge512);
+    std::generate(begin(z), end(z), getRandZ);
+
+    for (int i = 0; i < numKeys; ++i)
+    {
+        auto hilbertMixDKey = iHilbertMixD<unsigned>(x_le_511[i], y_le_511[i], z[i], bx, by, bz);
+        auto hilbertKey     = iHilbert<unsigned>(x_le_511[i], y_le_511[i], z[i]);
+
+        EXPECT_EQ(hilbertMixDKey, hilbertKey);
+    };
+
+    for (int i = 0; i < numKeys; ++i)
+    {
+        auto hilbertMixDKey      = iHilbertMixD<unsigned>(x_le_511[i], y_ge_512[i], z[i], bx, by, bz);
+        auto hilbertKey_py_m_512 = iHilbert<unsigned>(x_le_511[i], y_ge_512[i] - 512, z[i]);
+
+        EXPECT_EQ(hilbertMixDKey, (1 << 27) + hilbertKey_py_m_512);
+    };
+
+    for (int i = 0; i < numKeys; ++i)
+    {
+        auto hilbertMixDKey      = iHilbertMixD<unsigned>(x_ge_512[i], y_ge_512[i], z[i], bx, by, bz);
+        auto hilbertKey_py_m_512 = iHilbert<unsigned>(x_ge_512[i] - 512, y_ge_512[i] - 512, z[i]);
+
+        EXPECT_EQ(hilbertMixDKey, (2 << 27) + hilbertKey_py_m_512);
+    };
+
+    for (int i = 0; i < numKeys; ++i)
+    {
+        auto hilbertMixDKey      = iHilbertMixD<unsigned>(x_ge_512[i], y_ge_512[i], z[i], bx, by, bz);
+        auto hilbertKey_py_m_512 = iHilbert<unsigned>(x_ge_512[i] - 512, y_ge_512[i] - 512, z[i]);
+
+        EXPECT_EQ(hilbertMixDKey, (2 << 27) + hilbertKey_py_m_512);
+    };
 
     // clang-format off
     // iHilbertMixD(px in [0:512], py in [0:512], pz, bx, by, bz)       == iHilbert(px, py, pz) >> 3
     // iHilbertMixD(px in [0:512], py in [512:1024], pz, bx, by, bz)    == 01000000000 (=8^9) + (iHilbert(px, py - 512, pz) >> 3)
     // iHilbertMixD(px in [512:1024], py in [512:1024], pz, bx, by, bz) == 02000000000 (=8^9) + (iHilbert(px - 512, py - 512, pz) >> 3)
+    // IM: Can't understand below
     // iHilbertMixD(px in [0:512], py in [512:1024], pz, bx, by, bz)    == 03000000000 (=8^9) + (iHilbert(px, py - 512, pz) >> 3)
     // clang-format on
 }
